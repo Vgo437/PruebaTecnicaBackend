484f1ae5fc783e7b4421e2a042a17bd140c8e750
Vgo437 <vannegiraldoortiz437@gmail.com>
2026-08-06 09:52:30 -0500

    -Se agrega el servicio de bd 'postgres' -.env.example -endpoint /health/ready para validar la conexion de bd.

---
+++ backend/app/db/session.py
@@ -0,0 +1,12 @@
+from app.core.config import settings
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+
+
+engine = create_async_engine(settings.database_url, echo=True)
+
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from sqlalchemy import text
+from app.db.session import get_db
+from fastapi import FastAPI, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
-app = FastAPI(title="API de Solicitudes Institucionales", version="1.0.0")
+
+app = FastAPI(title="API de Solicitudes Institucionales para prueba tecnica", version="1.0.0")
 
 
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+
+@app.get("/health/ready")
+async def health_ready(db: AsyncSession = Depends(get_db)):
+    await db.execute(text("SELECT 1"))
+    return {"status": "ready"}
